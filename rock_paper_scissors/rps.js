1cf1c8abb7170384047c2bec526c65a73626a600
Bansker <jud.yannick@protonmail.ch>
2024-06-13 16:06:47 +0200

    Quick Commit

--- rock_paper_scissors/rps.js
+++ rock_paper_scissors/rps.js
@@ -1,19 +1,18 @@
+const rn        = Math.floor(Math.random() * 3);
+const rps       = ['scissors', 'paper', 'rock'];
+const usrin     = prompt('Rock Paper or Scissors').trim().toLowerCase();
+const rpsIndex  = rps.indexOf(usrin);
 
 
-const rn = Math.floor(Math.random() * 3);
-const rps = ['scissors', 'paper', 'rock'];
-console.log(rn);
-
-usrin = prompt('Rock Paper or Scissors').trim().toLowerCase();
-rpsIndex = rps.indexOf(usrin);
-console.log(rps.indexOf(usrin));
 
 console.log(`You chose ${usrin}\nComputer chose ${rps[rn]}`);
 
 if(rn == rpsIndex) {
   console.log('Its a draw...');
-} else if(rn < rpsIndex) {
+
+} else if((rpsIndex == rn + 1 ) || (rn == rpsIndex + 2)) {
   console.log('You Lost!');
+  
 } else {
   console.log('You Won!');
 }
