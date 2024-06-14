b1b568e75901c311a3d60df4fc15994fdd895ba9
Bansker <jud.yannick@protonmail.ch>
2024-06-14 07:41:51 +0200

    Quick Commit

--- playground/playground.js
+++ playground/playground.js
@@ -1,23 +1,7 @@
-const foo = (num) => {
-  if(num <= 11) {
-    return -1;
-  }
+const fooObj = {
+  rock:     'rock' | 0,
+  paper:    'paper' | 1,
+  scissors: 'scissors' | 2
+}
 
-  newNum = num.toString().split('');
-  indexes = num.toString().split('').length - 1;
-
-  for(let i = indexes; i >= 1; i--) {
-    if(newNum[i - 1] < newNum[i]) {
-      ls = newNum[i];
-      ms = newNum[i - 1];
-      newNum[i] = ms;
-      newNum[i - 1] = ls;
-
-      return Number(newNum.join(''));
-    } 
-  }
-
-  return -1;
-};
-
-console.log(foo(6566));
+console.log(fooObj[rock]);
