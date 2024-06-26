106ebc56031db68b1259411331f32c9139300875
Bansker <jud.yannick@protonmail.ch>
2024-06-26 14:24:57 +0200

    startet ui creation

--- rock_paper_scissors/rps.js
+++ rock_paper_scissors/rps.js
@@ -1,3 +1,21 @@
+const cardRock = document.querySelector('#rock');
+const cardPaper = document.querySelector('#paper');
+const cardScissors = document.querySelector('#scissors');
+const gameCard = document.querySelectorAll('.l-rps-card');
+
+gameCard.forEach((card) => {
+  console.log(card.id);
+
+
+  card.addEventListener('click', () => {
+    console.log(`${card.id}`);
+  });
+
+
+});
+
+
+/* 
 const rn        = Math.floor(Math.random() * 3);
 const rps       = ['scissors', 'paper', 'rock'];
 // const usrin     = prompt('Rock Paper or Scissors').trim().toLowerCase();
@@ -14,5 +32,6 @@ if(rn == rpsIndex) {
   console.log('You Lost!');
   
 } else {
-  console.log('You Won!');
+  console.log('You Won!'); 
 }
+ */
