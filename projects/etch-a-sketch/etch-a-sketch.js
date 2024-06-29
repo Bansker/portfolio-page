23c3c6b9f0c1ce40ea520c1ca5da3b5cee10cf83
Bansker <jud.yannick@protonmail.ch>
2024-06-29 15:09:58 +0200

    eas-js added some functionality

--- projects/etch-a-sketch/etch-a-sketch.js
+++ projects/etch-a-sketch/etch-a-sketch.js
@@ -4,37 +4,87 @@ const btnProgDarkening  = document.querySelector("#btn-mode-progressive-darkenin
 const btnClear          = document.querySelector("#btn-clear");
 
 const easContainer      = document.querySelector(".l-eas");
+const initialGridSize   = 16;
+const initBgColor       = "white";
+
+function getRandomRGBValue() {
+  return Math.floor(Math.random() * 256);
+}
+
+function initGrid(gridSize) {
+  for(let i = 0; i < (gridSize ** 2); i++) {
+    let gridTile = document.createElement("div")
+    
+    gridTile.setAttribute("style", `width: calc(100% / ${gridSize});`);
+    easContainer.appendChild(gridTile);
+  }
+}
+
+function easColoring(color) {
+  easContainer.addEventListener("mouseover", (elem) => {
+    elem.target.style.backgroundColor = color;
+  });
+}
+
+
+
+window.addEventListener("load", () => {
+  initGrid(initialGridSize);
+  easColoring("black");
+  console.log("Loaded grid");
+
+  
+});
+
+
 
 btnGridSize.addEventListener("click", () => {
-  return prompt("Enter Grid Size:");
+  const newGridSize = Number(prompt("Enter Grid Size:"));
+  
+  const easNodes = document.querySelector(".l-eas").children;
+  let currentTotalTiles = Object.keys(easNodes).length;
+
+  if((newGridSize ** 2) != currentTotalTiles) {
+    while(easContainer.firstChild){
+      easContainer.removeChild(easContainer.lastChild);
+    }
+  }
+
+  initGrid(newGridSize);
 });
+
+
 
 btnRandomColors.addEventListener("click", () => {
 
 });
 
+
+
 btnProgDarkening.addEventListener("click", () => {
 
 });
 
+
+
 btnClear.addEventListener("click", () => {
-  
+  const easNodes = document.querySelector(".l-eas").children;
+
+  for(let key in easNodes) {
+    easNodes[key].style.backgroundColor = initBgColor;
+  }
 });
 
-let gridSize = 15
-for(let i = 0; i < gridSize * gridSize; i++) {
-  let fragment = document.createElement("div")
-
-  fragment.setAttribute("style", 
-                        `width: calc(100% / ${gridSize});`);
-  easContainer.appendChild(fragment);
-}
 
 
 
 
-const easContainerFragments = document.querySelector(".l-eas");
-easContainerFragments.addEventListener("mouseover", (elem) => {
-  elem.target.style.backgroundColor = "red";
-});
+
+
+
+
+
+
+
+
 
