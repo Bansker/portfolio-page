74421f525d7863e5edd8395b88630076f2e1bbd3
Bansker <jud.yannick@protonmail.ch>
2024-07-11 15:49:36 +0200

    proj-calculator: refactoring

--- projects/calculator/scripts/calculator.js
+++ projects/calculator/scripts/calculator.js
@@ -1,3 +1,29 @@
+const Keyboard = {
+  btn_0 : document.querySelector('#btn-0'),
+  btn_1 : document.querySelector('#btn-1'),
+  btn_2 : document.querySelector('#btn-2'),
+  btn_3 : document.querySelector('#btn-3'),
+  btn_4 : document.querySelector('#btn-4'),
+  btn_5 : document.querySelector('#btn-5'),
+  btn_6 : document.querySelector('#btn-6'),
+  btn_7 : document.querySelector('#btn-7'),
+  btn_8 : document.querySelector('#btn-8'),
+  btn_9 : document.querySelector('#btn-9'),
+
+  btn_del : document.querySelector('#btn-del'),
+  btn_ac  : document.querySelector('#btn-ac'),
+  btn_mul : document.querySelector('#btn-mul'),
+  btn_div : document.querySelector('#btn-div'),
+  btn_add : document.querySelector('#btn-add'),
+  btn_sub : document.querySelector('#btn-sub'),
+  btn_dot : document.querySelector('#btn-dot'),
+  btn_exp : document.querySelector('#btn-exp'),
+  btn_ans : document.querySelector('#btn-ans'),
+  btn_eq  : document.querySelector('#btn-eq'),
+}
+
+
+/* 
 const btn_0 = document.querySelector('#btn-0');
 const btn_1 = document.querySelector('#btn-1');
 const btn_2 = document.querySelector('#btn-2');
@@ -19,3 +45,4 @@ const btn_dot = document.querySelector('#btn-dot');
 const btn_exp = document.querySelector('#btn-exp');
 const btn_ans = document.querySelector('#btn-ans');
 const btn_eq  = document.querySelector('#btn-eq');
+ */
