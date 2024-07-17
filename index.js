3e92cae5aa5109b0ac0e41076f7aa82971c3cea7
Bansker <jud.yannick@protonmail.ch>
2024-07-17 18:17:02 +0200

    cv-layout: implemented a basic mobile detection function based on the user agent string

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const plxWrapper = document.querySelector('.plx-wrapper');
 const out = document.querySelector('.js-out');
 const density = 200;
 
-const stickSrc = './svg/stick_smaller.svg';
-
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
 
-
+function isMobile() {
+  // const regex = /iphone|ipod|android|ie|blackberry|fennec/i
+  // return regex.test(navigator.userAgent);
+  return /Mobi/i.test(window.navigator.userAgent)
+}
 
 function getRandomSignedValue(range) {
   const randSign = Math.random() > 0.5 ? 1 : -1;
@@ -20,7 +22,10 @@ function getRandomValue(range) {
 }
 
 
+const mobile = isMobile();
 
+
+const stickSrc = './svg/stick_smaller.svg';
 let sticks = [];
 for(let i = 0; i < density; i++) {
   const randAngle  = getRandomValue(360);
@@ -43,47 +48,49 @@ for(let i = 0; i < density; i++) {
 }
 
 
-document.addEventListener('mousemove', (ev) => {
-  sticks.forEach((stick) => {
-    const position = stick.getAttribute('value');
-    const x = (window.innerWidth - ev.pageX * position) / 250;
-    const y = (window.innerHeight - ev.pageY * position) / 250;
-
-    // Shamelessly Stolen from https://stackoverflow.com/questions/59882504/how-to-get-style-transform-rotate-value-in-javascript
-    let angle = 0;
-    const rotate = stick.style.transform.match(/rotate\((\d+)(.+)\)/);
-    if (rotate) {
-      let [num, unit] = rotate.slice(1);  // slice is needed since first element contains entire match
-      //console.log('num:', num, 'unit:', unit);
-      angle = num;
-    }
-
-    stick.style.transform = `translateX(${x}px) translateY(${y}px) rotate(${angle}deg) scale(2)`;
+if(!mobile) {
+  document.addEventListener('mousemove', (ev) => {
+    sticks.forEach((stick) => {
+      const position = stick.getAttribute('value');
+      const x = (window.innerWidth - ev.pageX * position) / 250;
+      const y = (window.innerHeight - ev.pageY * position) / 250;
+  
+      // Shamelessly Stolen from https://stackoverflow.com/questions/59882504/how-to-get-style-transform-rotate-value-in-javascript
+      let angle = 0;
+      const rotate = stick.style.transform.match(/rotate\((\d+)(.+)\)/);
+      if (rotate) {
+        let [num, unit] = rotate.slice(1);  // slice is needed since first element contains entire match
+        //console.log('num:', num, 'unit:', unit);
+        angle = num;
+      }
+  
+      stick.style.transform = `translateX(${x}px) translateY(${y}px) rotate(${angle}deg) scale(2)`;
+    });
   });
-});
 
-
-window.addEventListener('deviceorientation', (ev) =>{
-  out.textContent = `Alpha: ${Math.round(ev.alpha)}, Beta: ${Math.round(ev.beta)}, Gamma: ${Math.round(ev.gamma)}`;
-
-  sticks.forEach((stick) => {
-    const position = stick.getAttribute('value');
-    const x = (window.innerWidth - ev.gamma * position) / 40;
-    const y = (window.innerHeight - ev.beta * position) / 40;
-
-    // Shamelessly Stolen from https://stackoverflow.com/questions/59882504/how-to-get-style-transform-rotate-value-in-javascript
-    let angle = 0;
-    const rotate = stick.style.transform.match(/rotate\((\d+)(.+)\)/);
-    if (rotate) {
-      let [num, unit] = rotate.slice(1);  // slice is needed since first element contains entire match
-      //console.log('num:', num, 'unit:', unit);
-      angle = num;
-    }
-
-    stick.style.transform = `translateX(${x}px) translateY(${y}px) rotate(${angle}deg) scale(2)`;
-
+} else {
+  window.addEventListener('deviceorientation', (ev) =>{
+    sticks.forEach((stick) => {
+      const position = stick.getAttribute('value');
+      const x = (window.innerWidth - ev.gamma * position) / 40;
+      const y = (window.innerHeight - ev.beta * position) / 40;
+  
+      // Shamelessly Stolen from https://stackoverflow.com/questions/59882504/how-to-get-style-transform-rotate-value-in-javascript
+      let angle = 0;
+      const rotate = stick.style.transform.match(/rotate\((\d+)(.+)\)/);
+      if (rotate) {
+        let [num, unit] = rotate.slice(1);  // slice is needed since first element contains entire match
+        //console.log('num:', num, 'unit:', unit);
+        angle = num;
+      }
+  
+      stick.style.transform = `translateX(${x}px) translateY(${y}px) rotate(${angle}deg) scale(2)`;
+  
+    });
   });
-});
+
+}
+
 
 
 
