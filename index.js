2bf6a3a4084be0981a2fdfa580b4da0f2c5c1401
Bansker <jud.yannick@protonmail.ch>
2024-07-18 12:57:38 +0200

    cv-layout: cleaned up code, event listener for parallax effect depends now on device. added depth scaling

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const plxWrapper = document.querySelector('.plx-wrapper');
-const out = document.querySelector('.js-out');
-const density = 200;
 
-const windowWidth = window.innerWidth;
-const windowHeight = window.innerHeight;
+
+
 
 function isMobile() {
   // const regex = /iphone|ipod|android|ie|blackberry|fennec/i
@@ -22,10 +19,16 @@ function getRandomValue(range) {
 }
 
 
-const mobile = isMobile();
+const mobile        = isMobile();
+const plxWrapper    = document.querySelector('.plx-wrapper');
+const density       = 200;
+const windowWidth   = window.innerWidth;
+const windowHeight  = window.innerHeight;
 
 
 const stickSrc = './svg/stick_smaller.svg';
+const stickColor = `invert(61%) sepia(46%) saturate(5056%) hue-rotate(2deg) brightness(102%) contrast(101%)`;
+
 let sticks = [];
 for(let i = 0; i < density; i++) {
   const randAngle  = getRandomValue(360);
@@ -33,58 +36,55 @@ for(let i = 0; i < density; i++) {
   const randPositionTop = getRandomValue(100);
   const randPositionLeft = getRandomValue(100);
 
+  const elemPositionalValue = getRandomSignedValue(30);
+  const elemDepthScale      = elemPositionalValue / 10;
+  const elemScale           = 2 + elemDepthScale;
+
   sticks[i] = document.createElement('img');
   sticks[i].src = stickSrc;
 
-  sticks[i].setAttribute('value', `${getRandomSignedValue(30)}`);
-
-  sticks[i].setAttribute('style', `position: absolute;
-                                   top: ${randPositionTop}%;
-                                   left: ${randPositionLeft}%;
-                                   transform: rotate(${randAngle}deg) scale(2);
-                                   filter: invert(61%) sepia(46%) saturate(5056%) hue-rotate(2deg) brightness(102%) contrast(101%);`);
+  sticks[i].setAttribute('value', `${elemPositionalValue}`);
+  sticks[i].style.position  = 'absolute';
+  sticks[i].style.top       = `${randPositionTop}%`;
+  sticks[i].style.left      = `${randPositionLeft}%`;
+  sticks[i].style.transform = `rotate(${randAngle}deg) scale(${elemScale})`;
+  sticks[i].style.filter    = `${stickColor}`;
   
   plxWrapper.appendChild(sticks[i]);
 }
 
 
-if(!mobile) {
-  document.addEventListener('mousemove', (ev) => {
-    sticks.forEach((stick) => {
-      const position = stick.getAttribute('value');
-      const x = (window.innerWidth - ev.pageX * position) / 250;
-      const y = (window.innerHeight - ev.pageY * position) / 250;
-  
-      // Shamelessly Stolen from https://stackoverflow.com/questions/59882504/how-to-get-style-transform-rotate-value-in-javascript
-      let angle = 0;
-      const rotate = stick.style.transform.match(/rotate\((\d+)(.+)\)/);
-      if (rotate) {
-        let [num, unit] = rotate.slice(1);  // slice is needed since first element contains entire match
-        //console.log('num:', num, 'unit:', unit);
-        angle = num;
-      }
-  
-      stick.style.transform = `translateX(${x}px) translateY(${y}px) rotate(${angle}deg) scale(2)`;
-    });
-  });
 
-} else {
-  window.addEventListener('deviceorientation', (ev) =>{
-    sticks.forEach((stick) => {
-      const position = stick.getAttribute('value');
-      const x = (window.innerWidth - ev.gamma * position) / 40;
-      const y = (window.innerHeight - ev.beta * position) / 40;
-  
-      // Shamelessly Stolen from https://stackoverflow.com/questions/59882504/how-to-get-style-transform-rotate-value-in-javascript
-      let angle = 0;
-      const rotate = stick.style.transform.match(/rotate\((\d+)(.+)\)/);
-      if (rotate) {
-        let [num, unit] = rotate.slice(1);  // slice is needed since first element contains entire match
-        //console.log('num:', num, 'unit:', unit);
-        angle = num;
-      }
-  
-      stick.style.transform = `translateX(${x}px) translateY(${y}px) rotate(${angle}deg) scale(2)`;
-    });
+const eventListener = mobile ? 'deviceorientation' : 'mousemove';
+
+document.addEventListener(eventListener , (ev) => {
+  sticks.forEach((stick) => {
+    const position       = stick.getAttribute('value');
+
+    const devicePosXValue = mobile ? ev.gamma : ev.pageX;
+    const devicePosYValue = mobile ? ev.beta  : ev.pageY;
+
+    const x = (window.innerWidth - devicePosXValue * position) / 250;
+    const y = (window.innerHeight - devicePosYValue * position) / 250;
+
+    const elemDepthScale      = position / 10;
+    const elemScale           = 2 + elemDepthScale;
+
+
+
+    // Shamelessly Stolen from https://stackoverflow.com/questions/59882504/how-to-get-style-transform-rotate-value-in-javascript
+    let angle = 0;
+    const rotate = stick.style.transform.match(/rotate\((\d+)(.+)\)/);
+    if (rotate) {
+      let [num, unit] = rotate.slice(1);  // slice is needed since first element contains entire match
+      //console.log('num:', num, 'unit:', unit);
+      angle = num;
+    }
+
+    stick.style.transform = `translate(${x}px, ${y}px) rotate(${angle}deg) scale(${elemScale})`;
+
   });
-}
+});
+
+
+
